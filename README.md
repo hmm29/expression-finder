28b5c1f7e0be2344db9b38c2e93864cd242589cf
Harrison Miller <harrison.miller@yale.edu>
2017-05-27 23:46:33 -0400

    add and update jsDoc comments, reorder functions, and make comments clearer

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ The given problem was the following:
 
 "Use each of the numbers 1,3,4, and 6 exactly once with any of the four basic math operations (addition, subtraction, multiplication, and division) to total 24. Each number must be used once and only once, and you may define the order of operations; for example, 3 * (4 + 6) + 1 = 31 is valid, however incorrect, since it doesn't total 24."
 
-I initially guessed that there was no solution, but I really wanted to make sure that my answer was correct, so I coded up this program. It works for all inputs of four numbers, uses the four arithmetic operators, and knows how to apply order of operations. I had fun making this too.
+I initially guessed that there was no solution to the above problem, but I really wanted to make sure that my answer was correct, so I coded up this program. It works for all inputs of four nonnegative numbers, uses the four arithmetic operators, and knows how to apply order of operations. I had fun making this too.
--- findExpression.js
+++ findExpression.js
@@ -6,8 +6,8 @@
  * @param {number} value - the value of the expression result
  * @returns {?string} stringExp - the expression that evaluates to value
  */
-
 function findExpression(nums, ops, value) {
+  // error handling
   if(!Array.isArray(nums) || nums.length !== 4 || !Array.isArray(ops) || ops.length > 4 || typeof value !== "number") {
     throw new Error('invalid inputs');
   }
@@ -15,33 +15,36 @@ function findExpression(nums, ops, value) {
   // generate all permutations of nums
   let perms = generatePermutations(nums.map((num) => num + ""));
 
-  // Expression string format:
-  // operands at indices 1, 4, 8, and 11
-  // operators at indices 2, 6, and 10
-  // parentheses pairs at indices (0,5), (3,9), and (7,12)
-
+  // turn the permutations into formatted arrays filled only with operands
   let expsWithOperands = perms.map((perm) => makeExpWithOperands(perm)),
-      stringExp = '',
-      parPairs = [[0,5],[3,9],[7,12]];
+      stringExp = '';
+
+  // parentheses pairs go at index pairs (0,5), (3,9), and (7,12)
+  const parPairs = [[0,5],[3,9],[7,12]];
 
   for(let exp of expsWithOperands) {
     for(let i = 0; i < ops.length; i++) {
       for(let j = i; j < ops.length; j++) {
         for(let k = j; k < ops.length; k++) {
+          // place operators at indices 2, 6, and 10 of expression array
           exp[2] = ops[i];
           exp[6] = ops[j];
           exp[10] = ops[k];
 
+          // insert opening and closing parentheses at each index pair to determine order of operations
           for(let i = 0; i < parPairs.length; i++) {
             exp[parPairs[i][0]] = '(';
             exp[parPairs[i][1]] = ')';
 
+            // join formatted expression array into string
             stringExp = exp.join('');
+
+            // evaluate the string expression and compare the result to value
             if(eval(stringExp) === value) {
               return stringExp;
             }
 
-            // clear the parentheses pair
+            // clear the current parentheses pair
             exp[parPairs[i][0]] = '';
             exp[parPairs[i][1]] = '';
           }
@@ -53,21 +56,11 @@ function findExpression(nums, ops, value) {
   return null;
 }
 
-function makeExpWithOperands(perm) {
-  if(typeof perm !== 'string') {
-    return null;
-  }
-
-  let exp = [];
-  let operandIdxs = [1,4,8,11];
-
-  for(let i = 0; i < perm.length; i++) {
-     exp[operandIdxs.shift()] = perm[i];
-  }
-
-  return exp;
-}
-
+/**
+ * Finds all permutations of characters in an array.
+ * @param {Array} arr - an array of four characters
+ * @returns {String[]} output - an array of all permutations of the characters in arr
+ */
 function generatePermutations(arr) {
   if(arr.length <= 1) {
     return arr;
@@ -83,6 +76,26 @@ function generatePermutations(arr) {
   }
 
   return output;
+}
+
+/**
+ * Makes a formatted expression array from a string of four numbers.
+ * @param {string} perm - a permutation of four numbers
+ * @returns {Array} exp - an array containing the numbers at indices 1, 4, 8, and 11
+ */
+function makeExpWithOperands(perm) {
+  if(typeof perm !== 'string') {
+    return null;
+  }
+
+  let exp = [];
+  let operandIdxs = [1,4,8,11];
+
+  for(let i = 0; i < perm.length; i++) {
+     exp[operandIdxs.shift()] = perm[i];
+  }
+
+  return exp;
 }
 
 findExpression([1,3,4,6],['+','-','*','/'],24); // null
